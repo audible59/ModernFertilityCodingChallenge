dc3800cf078fbdf926f3e7c8c6e846984aeed802
Kevin E. Rafferty II <kevin@scanwellhealth.com>
2022-01-28 12:37:50 -0800

    [KERII] Initial Project setup.

---
+++ ModernFertility/ModernFertility/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernFertility
+//
+//  Created by Kevin E. Rafferty II on 1/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
